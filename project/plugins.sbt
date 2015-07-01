109a3184683429a93c7c61fe63f3e6df6d89f4a7
Andrea <andrea.peruffo1982@gmail.com>
2015-07-01 16:37:32 +0200

    added license

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,3 @@
-addSbtPlugin("eu.unicredit" % "sbt-swagger-codegen" % "0.0.2-SNAPSHOT")
+addSbtPlugin("eu.unicredit" % "sbt-swagger-codegen" % "0.0.3-SNAPSHOT")
 
-//addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.0")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.0")
--- src/main/scala/eu/unicredit/PetStoreControllerImpl.scala
+++ src/main/scala/eu/unicredit/PetStoreControllerImpl.scala
@@ -1,3 +1,17 @@
+/* Copyright 2015 UniCredit S.p.A.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package eu.unicredit
 
 import play.api.mvc.{ Request, AnyContent }
@@ -15,7 +29,7 @@ import play.api.libs.concurrent.Execution.Implicits._
 /*
 
 YOU are in charge of providing this implementation
-
+*/
 trait PetStoreControllerImpl {
 
   def onError(s : String,err: Throwable) =
@@ -91,6 +105,7 @@ swaggerServerAsync := true
     pets.find(_.id == id)
   }
 */
-}
 
+}
+/*
 */
